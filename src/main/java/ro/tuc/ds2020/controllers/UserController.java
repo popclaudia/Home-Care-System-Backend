b4243e1eccb42d41aadf30a4278329abe072f2fa
Pop Claudia Dorina <pop_claudia_dorina@yahoo.com>
2021-01-13 21:26:06 +0000

    Update UserController.java

--- src/main/java/ro/tuc/ds2020/controllers/UserController.java
+++ src/main/java/ro/tuc/ds2020/controllers/UserController.java
@@ -23,7 +23,7 @@ import java.util.Objects;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/api/user")
 public class UserController {
 
     private final UserService userService;
